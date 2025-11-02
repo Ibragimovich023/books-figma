084378adf404a9b954208b9bfe3ff6ef0690d35a
Shaxboz <shaxboz@example.com>
2025-11-02 20:45:55 +0500

    book

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+import category from "../assets/category.png";
+const categoryData = [
+  {
+    id: 1,
+    title: "Жахон адабиёти",
+    img: category,
+  },
+  {
+    id: 2,
+    title: "Узбек адабиёти",
+    img: category,
+  },
+  {
+    id: 3,
+    title: "Бизнес ва психология",
+    img: category,
+  },
+  {
+    id: 4,
+    title: "Болалар адабиёти ",
+    img: category,
+  },
+  {
+    id: 5,
+    title: "Детективлар",
+    img: category,
+  },
+  {
+    id: 6,
+    title: "Фантастика",
+    img: category,
+  },
+];
+
+export { categoryData };
